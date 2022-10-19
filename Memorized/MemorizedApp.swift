e14445671606568eabb54c3c10a1fdb8c04193a6
ViniTamanhao <105015723+ViniTamanhao@users.noreply.github.com>
2022-10-19 15:05:49 -0300

    Initial Commit

---
+++ Memorized/MemorizedApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizedApp.swift
+//  Memorized
+//
+//  Created by Vinícius Tamanhão on 19/10/22.
+//
+
+import SwiftUI
+
+@main
+struct MemorizedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
